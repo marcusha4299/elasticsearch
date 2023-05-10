22b047f75b177027bad1f40afce7e47d39d5331c
Nguyen Ha <nguyeh3@circinus-3.ics.uci.edu>
2023-05-09 18:15:35 -0700

    First Committed

---
+++ invertedIndex.py
@@ -0,0 +1,8 @@
+class InvertedIndex:
+    def add_document(self, doc_name, tokens): #Add document name/id to the list of documents
+        
+    def get_posting(self, term): #return a posting list for a given term
+        
+    def get_document_frequency(self, term): #return document frequency 
+        
+    def check_tfidf(self, doc_name, term): #check the tf-idf score
